691d0172a9e54fff3f23b4e99184f27c7b7bc200
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-05-07 01:30:29 +0500

    File of q98

---
+++ q98.js
@@ -0,0 +1,10 @@
+//Create a JavaScript snippet that calculates and logs how many days are left until New Year.
+// Calculates how many days are left until New Year's Day
+function daysUntilNewYear() {
+    var today = new Date();
+    var newYear = new Date(today.getFullYear() + 1, 0, 1); // January 1st of next year
+    var diff = newYear.getTime() - today.getTime(); // Difference in milliseconds
+    var days = Math.ceil(diff / (1000 * 60 * 60 * 24)); // Converts to days
+    return days;
+}
+console.log(daysUntilNewYear() + "days until New Year.");
